c52f8da403f0fca5ca5c3ecfc46c45bf473a8109
Ivan Morozov <ivan.morozov@kreditech.com>
2017-02-18 19:29:27 +0100

    enabled tests for new method

--- src/main/scala/io/allquantor/SpaceLaserAreaCalculator.scala
+++ src/main/scala/io/allquantor/SpaceLaserAreaCalculator.scala
@@ -9,21 +9,21 @@ object SpaceLaserAreaCalculator {
   /**
     *
     * @param previous previous Building in the left
-    * @param current current looked Building
+    * @param current  current looked Building
     * @return area that could be probably covered with water and should be subtracted later
     */
-  def `addToLocalArea in case of overflood`(previous:Building, current:Building): Area = {
+  def `addToLocalArea in case of overflood`(previous: Building, current: Building): Area = {
     calcArea(previous, current) + calcArea(current)
   }
 
   /**
     *
-    * @param highest the current highest building
-    * @param current the current considered building
+    * @param highest   the current highest building
+    * @param current   the current considered building
     * @param localArea the state of the current localArea
     * @return the value should be added to the current area in case when new boundary (highest or second highest) is found.
     */
-  def `update Area when new Boundary found`(highest:Building, current: Building, localArea: Area): Area = {
+  def `update Area when new Boundary found`(highest: Building, current: Building, localArea: Area): Area = {
     calcArea(highest, current) - localArea
   }
 
@@ -73,7 +73,7 @@ object SpaceLaserAreaCalculator {
         println("current is bigger than second highest")
         println(s"current:{$current}")
         println(s"second highest:{$secondHighest}")
-        area += `update Area when new Boundary found`(highest, current,localArea)
+        area += `update Area when new Boundary found`(highest, current, localArea)
         println(s"area is set to $area")
         localArea = `addToLocalArea in case of overflood`(highest, current)
         println(s"local area is set to $localArea")
@@ -84,7 +84,7 @@ object SpaceLaserAreaCalculator {
         println(s"highest $highest")
         println(s"calculate the area between highest and curent ${calcArea(highest, current)}")
         println(s"decreasing the local area ${localArea}")
-        area += `update Area when new Boundary found`(highest, current,localArea)
+        area += `update Area when new Boundary found`(highest, current, localArea)
         println(s"area is set to $area")
         highest = current
         println(s"new highest is set to ${highest}")
@@ -96,6 +96,78 @@ object SpaceLaserAreaCalculator {
       }
     }
     area
+  }
+
+  def calculate(s: Seq[Building]): Area = {
+
+    /**
+      *
+      * @param first  first left Building.
+      * @param second second Building.
+      * @return Highest,SecondHighest,LocalArea,GlobalArea
+      */
+    def initials(first: Building, second: Building): (Building, Building, Area, Area) = {
+      // Local Area is the term describing the factor what should be decreased from the global area
+      // in case if we have the case of overflooding of a part on the graph.
+      var initLocalArea = calcArea(first, second)
+      val initArea = calcArea(first, second)
+
+      // Since in the iteration we need to keep track of the current highest
+      // and the second highest building. We distinguish here between the first and second.
+      val initHighest = if (first > second) first else second
+
+      val initSecondHighest = if (first > second) {
+        // In case that the first building is bigger than the second,
+        // we have the chance that the second will be flooded.
+        // We add the area of the second building to the local area
+        // to ensure the subtraction later is correct.
+        initLocalArea += calcArea(second)
+        second
+      } else {
+        first
+      }
+      (initHighest, initSecondHighest, initLocalArea, initArea)
+    }
+
+    /**
+      *
+      * @param s   sequence
+      * @param pos current position of the pointer
+      * @return (Previous,Current,Next)
+      */
+    def iterationState(s: IndexedSeq[Building], pos: Int): (Building, Building, Building) = {
+      (s(pos), Try(s(pos - 1)).toOption.getOrElse(s.tail.head), Try(s(pos + 1)).toOption.getOrElse(s(pos)))
+    }
+
+
+    s match {
+      case Nil => 0
+      case _ :: Nil => 0
+      case first :: second :: Nil => calcArea(first, second)
+      case first :: second :: next :: tail => {
+        var (high, sHigh, local, global) = initials(first, second)
+        val start = next :: tail toIndexedSeq
+
+        for (i <- start.indices) {
+          var (current,previous,next) = iterationState(start,i)
+
+          if (current < high && current <= sHigh) {
+            local += `addToLocalArea in case of overflood`(previous, current)
+            global += calcArea(previous, current)
+          } else if(current < high && current > sHigh) {
+            global += `update Area when new Boundary found`(high, current, local)
+            local = `addToLocalArea in case of overflood`(high, current)
+            sHigh = current
+          } else if(current >= high) {
+            global += `update Area when new Boundary found`(high, current, local)
+            high = current
+            sHigh = next
+            local = 0
+          }
+        }
+        global
+      }
+    }
   }
 
 }
--- src/test/scala/CalculationSpec.scala
+++ src/test/scala/CalculationSpec.scala
@@ -9,28 +9,26 @@ class CalculationSpec extends FlatSpec with Matchers {
   "Space laser area calculator" should
     "calculate the area of water" in {
     val simpleSkyline = Seq(Building(1, 2, 5), Building(3, 4, 1))
-    // val simpleSkylineResult = SpaceLaserAreaCalculator.calculate(simpleSkyline) should be(1)
+    val simpleSkylineResult = SpaceLaserAreaCalculator.calculate(simpleSkyline) should be(1)
 
     // Ensure that reordering breaks the sequence.
     val reversedSkyline = simpleSkyline.reverse
-    //    simpleSkylineResult should not be SpaceLaserAreaCalculator.calculate(reversedSkyline)
+    simpleSkylineResult should not be SpaceLaserAreaCalculator.calculate(reversedSkyline)
 
     // edge cases for empty and singleton
-    //SpaceLaserAreaCalculator.calculate(Nil) should be (0)
-    //SpaceLaserAreaCalculator.calculate(Seq(Building(1,1,1))) should be (0)
+    SpaceLaserAreaCalculator.calculate(Nil) should be(0)
+    SpaceLaserAreaCalculator.calculate(Seq(Building(1, 1, 1))) should be(0)
 
     val smallerFirst = Seq(Building(5, 9, 2), Building(15, 16, 50))
     // (15 - 9) * min(2,50) == 12
-    //SpaceLaserAreaCalculator.calculate(smallerFirst) should be(12)
+    SpaceLaserAreaCalculator.calculate(smallerFirst) should be(12)
 
-    val triple = simpleSkyline :+ smallerFirst.head
-    val tripleResult = (5 - 2) * 2 - calcArea(Building(3, 4, 1))
-    //SpaceLaserAreaCalculator.calculate(triple) should be(tripleResult)
 
     val combined = simpleSkyline ++ smallerFirst
     val combinedAreaResult = ((15 - 2) * 5) - (calcArea(Building(3, 4, 1)) + calcArea(Building(5, 9, 2)))
 
-    SpaceLaserAreaCalculator.cal2(combined) should be(combinedAreaResult)
+
+    SpaceLaserAreaCalculator.calculate(combined) should be(combinedAreaResult)
 
 
     val q = Seq(Building(1, 3, 2), Building(4, 5, 1), Building(6, 8, 10), Building(9, 13, 15), Building(15, 17, 2))
@@ -39,7 +37,8 @@ class CalculationSpec extends FlatSpec with Matchers {
       calcArea(Building(1, 3, 2), Building(6, 8, 10)) +
       calcArea(Building(9, 13, 15), Building(15, 17, 2))) -
       calcArea(Building(4, 5, 1))
-    SpaceLaserAreaCalculator.cal2(q) should be(qres)
+
+    SpaceLaserAreaCalculator.calculate(q) should be(qres)
 
 
     val qw = Seq(Building(1, 3, 2), Building(4, 5, 1), Building(6, 8, 10), Building(9, 13, 15), Building(15, 17, 2), Building(19, 21, 3))
@@ -56,7 +55,7 @@ class CalculationSpec extends FlatSpec with Matchers {
       calcArea(Building(15, 17, 2))
 
 
-    SpaceLaserAreaCalculator.cal2(qw) should be(qwres)
+    SpaceLaserAreaCalculator.calculate(qw) should be(qwres)
 
   }
 
